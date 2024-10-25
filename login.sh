59c6cb5ee20c4f0bb52d7a5abe20322ca1a53a61
wr-tiger <ikikopok1410@gmail.com>
2024-10-25 08:14:51 +0700

    update

--- login.sh
+++ login.sh
@@ -13,15 +13,32 @@ while true; do
   read pwd
   if [ "$pwd" == "$correct_password" ]; then
     echo -e "${green}akses di berikan!"
-echo -e "${yellow}______________________________________________________"
-sleep 1
-echo -e "${yellow} (+)  git clone https://github.com/wr-tiger/TGR-tools.git "
-sleep 2
-echo -e "${yellow} (+)  https://www.mediafire.com/file/3tng6z5dogsnsli/text.txt/file "
-sleep 2
-echo -e "${yellow}______________________________________________________"
+    echo -e "${yellow}______________________________________________________"
+    sleep 1
+    echo -e "${yellow} (+)  git clone https://github.com/wr-tiger/TGR-tools.git "
+    echo -e "${yellow} (+)  https://www.mediafire.com/file/3tng6z5dogsnsli/text.txt/file "
+    echo -e "${yellow}______________________________________________________"
+    sleep 2
+
+    # Menambahkan menu pilihan
+    echo -e "${cyan}Pilih opsi berikut:"
+    echo -e "${purple}1) Opsi 1: Jalankan git clone"
+    echo -e "${purple}2) Opsi 2: Buka link mediafire"
+    echo -ne "${blue}Pilih opsi>> "
+    read option
+
+    # Menggunakan if untuk memproses pilihan
+    if [ "$option" == "1" ]; then
+      echo -e "${green}Menjalankan git clone..."
+      git clone https://github.com/wr-tiger/TGR-tools.git
+    elif [ "$option" == "2" ]; then
+      echo -e "${green}Membuka link mediafire..."
+      xdg-open "https://www.mediafire.com/file/3tng6z5dogsnsli/text.txt/file"
+    else
+      echo -e "${red}Pilihan tidak valid."
+    fi
     break
   else
-    echo -e "${red}akses di tolak password salah"
+    echo -e "${red}akses di tolak, password salah"
   fi
 done
